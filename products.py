6612597120c65158802a0097f141a5bc93d18cb4
NikhilNattava98 <83455400+NikhilNattava98@users.noreply.github.com>
2023-05-13 18:44:17 -0500

    Update products.py

--- products.py
+++ products.py
@@ -1,6 +1,7 @@
 import sqlite3
 import tkinter as tk
 from tkinter import messagebox
+import mysql.connector
 
 # Create the main window
 root = tk.Tk()
@@ -34,7 +35,7 @@ def delete_product_detail():
     mydb = mysql.connector.connect(host='localhost', port=3306, user='root', password='admin', database='ecommerce')
     c = mydb.cursor()
     product_id = product_id_var.get()
-    c.execute("DELETE FROM products WHERE order_detail_id=%s", (product_id))
+    c.execute("DELETE FROM products WHERE product_id=%s", (product_id,))
     mydb.commit()
     messagebox.showinfo('Success', 'Product detail deleted successfully.')
 
@@ -49,23 +50,23 @@ def view_product_details():
 
 
 # Create the input fields
-order_detail_id_label = tk.Label(root, text='Product ID')
-order_detail_id_label.grid(row=0, column=0)
-order_detail_id_var = tk.StringVar()
-order_detail_id_entry = tk.Entry(root, textvariable=product_id_var.get())
-order_detail_id_entry.grid(row=0, column=1)
-
-order_id_label = tk.Label(root, text='Product Name')
-order_id_label.grid(row=1, column=0)
-order_id_var = tk.StringVar()
-order_id_entry = tk.Entry(root, textvariable=product_name)
-order_id_entry.grid(row=1, column=1)
-
-product_id_label = tk.Label(root, text='manufacturer')
-product_id_label.grid(row=2, column=0)
+product_id_label = tk.Label(root, text='Product ID')
+product_id_label.grid(row=0, column=0)
 product_id_var = tk.StringVar()
-product_id_entry = tk.Entry(root, textvariable=manufacturer)
-product_id_entry.grid(row=2, column=1)
+product_id_entry = tk.Entry(root, textvariable=product_id_var)
+product_id_entry.grid(row=0, column=1)
+
+product_name_label = tk.Label(root, text='Product Name')
+product_name_label.grid(row=1, column=0)
+product_name_var = tk.StringVar()
+product_name_entry = tk.Entry(root, textvariable=product_name_var)
+product_name_entry.grid(row=1, column=1)
+
+manufacturer_label = tk.Label(root, text='manufacturer')
+manufacturer_label.grid(row=2, column=0)
+manufacturer_var = tk.StringVar()
+manufacturer_entry = tk.Entry(root, textvariable=manufacturer_var)
+manufacturer_entry.grid(row=2, column=1)
 
 
 # Create the buttons
